4e19d14b9beb585a351ceed2b46831815ff10322
polestar <63390979+parkkyusang@users.noreply.github.com>
2026-02-20 17:53:38 +0900

    Add files via upload

--- articles.js
+++ articles.js
@@ -1,8 +1,13 @@
 // articles.js
 const articles = [
     {
-        title: "[칼럼] 피지컬 훈련이 솔랭 티어를 올리는 진짜 이유 (인지과학)",
-        link: "column_01.html",  // ← 방금 만든 파일 이름으로 변경
-        status: "NEW"            // ← 뱃지도 눈에 띄게 변경
+        title: "[제작 의도] 왜 한타만 되면 손이 꼬일까? : 순간적 인지 과부하와 피지컬 트레이닝",
+        link: "column_02.html",
+        status: "필독"
+    },
+    {
+        title: "[칼럼] 피지컬 훈련이 솔랭 티어를 올리는 진짜 이유 (feat. 인지과학)",
+        link: "column_01.html",
+        status: "NEW"
     }
 ];
